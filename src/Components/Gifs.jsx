471cbe210e856f4ae62ae9de82c12090dc41c3fa
Deepanshu Tiwari <ddepu11@gmail.com>
2021-03-25 09:14:25 +0530

    avatar done

--- src/Components/Gifs.jsx
+++ src/Components/Gifs.jsx
@@ -1,4 +1,6 @@
 import { useGlobalContext } from "../context";
+import Loading from "./Loading";
+import { FcBusinessman } from "react-icons/fc";
 
 const Gifs = () => {
   const { gifs } = useGlobalContext();
@@ -6,28 +8,37 @@ const Gifs = () => {
   console.log(gifs);
 
   let showGifs =
-    gifs.length !== 0
-      ? gifs.map((item, index) => {
-          const {
-            id,
-            images: {
-              downsized_large: { url },
-            },
-            user: { banner_url },
-            username,
-          } = item;
+    gifs.length !== 0 ? (
+      gifs.map((item, index) => {
+        let {
+          id,
+          images: {
+            downsized_large: { url },
+          },
+          user: { banner_url },
+          username,
+          trending_datetime,
+        } = item;
+        trending_datetime = new Date(trending_datetime).toLocaleDateString();
+        return (
+          <a href="/a" key={index} className="gif card">
+            <img src={url} alt="" />
+            <div className="info">
+              {banner_url !== "" ? (
+                <img src={banner_url} alt="s" className="avatar" />
+              ) : (
+                <FcBusinessman className="avatar" />
+              )}
 
-          return (
-            <a href="/a" key={index} className="gif card">
-              <img src={url} alt="" />
-              <div className="info">
-                <img src={banner_url} alt="" />
-                <p>{username}</p>
-              </div>
-            </a>
-          );
-        })
-      : "";
+              <p className="us_name">{username}</p>
+              <span className="tr_time">{trending_datetime}</span>
+            </div>
+          </a>
+        );
+      })
+    ) : (
+      <Loading />
+    );
   return <main className="hero">{showGifs}</main>;
 };
 export default Gifs;
