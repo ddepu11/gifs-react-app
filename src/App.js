37ee14d5b42fe2cbec1163c39de156e1c26d617c
Deepanshu Tiwari <ddepu11@gmail.com>
2021-03-26 10:33:26 +0530

    resolving blank page issue 2

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@ import Gifs from "./Components/Gifs";
 import Gif from "./Components/Gif";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-
 function App() {
-
   return (
     <div className="container">
       <Router>
-
         <Switch>
-          <Route path="/" exact>
+          <Route path="/" exact={true}>
             <Gifs />
           </Route>
 
